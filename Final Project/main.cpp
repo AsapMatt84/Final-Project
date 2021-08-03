827432311679484acbd21b68917821b95f5b4316
AsapMatt84 <matthew.cahill@sinclair.edu>
2021-08-03 19:36:25 -0400

    Initial Commit

---
+++ Final Project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Final Project
+//
+//  Created by Matt Cahill on 8/3/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
